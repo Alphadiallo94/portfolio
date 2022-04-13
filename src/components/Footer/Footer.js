0e893af03f442ee09d2fd927b0743b745e5f7ae7
Alphadiallo94 <alphaoumar94@hotmail.fr>
2022-04-13 18:30:59 +0200

    new version2

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -13,7 +13,7 @@ function Footer() {
         <div className="footer" style={{backgroundColor: theme.secondary}}>
             <p style={{color: theme.tertiary}}>
                 { <span style={{color: theme.primary, margin: '0 0.5rem -1rem 0.5rem'}}>
-                    Alpha ❤
+                    
                 </span>/*
                  by   {shortname( HeaderData.name)}
                  */
--- src/data/AboutData.js
+++ src/data/AboutData.js
@@ -2,8 +2,8 @@ export const AboutData = {
     title: "Qui suis-je ?",
     description1: "Bonjour, moi c'est Alpha DIALLO",
     //description2: "Très Altruiste, et passioné de finance je me caractérise comme une personne calme qui aime se cultiver. J'aime apprendre de nouvelles choses et aujourd'hui l'objectif ultime serait de pouvoir intégrer et représenter une entreprise à travers des valeurs qui me représentent.",
-    description2:"Altruiste, passionné de nouvelles technologies et de finance, j'arrive à la fin de mes études et de mon contrat d'apprentissage ou j'ai commencé à acquérir un large panel de compétence.",
-    description3:"Aujourd'hui, dans mon désir d'apprentissage, et dans ma volonté d'apporter ma contribution dans la transformation numérique pour tout type d'entité j'ai eu des interrogations et aujourd'hui votre entreprise est celle qui regroupe le plus mes attentes.",
+    description2:"Altruiste, passionné de nouvelles technologies et de finance, j'arrive à la fin de mes études et de mon contrat d'apprentissage où j'ai commencé à acquérir un large panel de compétences.",
+    description3:"Aujourd'hui, dans mon désir d'apprentissage, et dans ma volonté d'apporter ma contribution face à cet essor du numérique j'ai eu des interrogations et aujourd'hui votre entreprise est celle qui répond le plus mes attentes.",
 
     image: 2
 }
